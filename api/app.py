b57d3cf8033f5d06fb37a6c4f5a0d6e938ebd4da
Kamil Kowalski <kamilkowalski314@gmail.com>
2021-12-12 03:24:12 +0100

    Changed table structure, improved authentication, working table fetcg

--- api/app.py
+++ api/app.py
@@ -25,20 +25,13 @@ class LoginData(BaseModel):
 class TokenData(BaseModel):
     token: str
 
-class TableRequest(BaseModel):
-    month: str
-    year: str
 
+def getDb():
+    return mysql.connector.connect(host="localhost",port=3307,database="Navigator",user="root",password="plsnohack")
 def getUserFromToken(token: str):
-    session = mysql.connector.connect(
-        host="localhost",
-        port=3307,
-        database="Navigator",
-        user="root",
-        password="plsnohack"
-    )
+    session = getDb()
     cursor = session.cursor()
-    sqlQuery = "SELECT userId FROM Sessions WHERE token=%s "#" AND timestampdiff(HOUR,Sessions.loginDate,NOW())<2"
+    sqlQuery = "SELECT userId FROM Sessions WHERE token=%s AND timestampdiff(HOUR,Sessions.loginDate,NOW())<2 AND active=1"
     cursor.execute(sqlQuery,(token,))
     rows = cursor.fetchall()
     if len(rows)==1:
@@ -53,35 +46,56 @@ def hello_world():
 @app.post("/verification")
 def verify_user(loginData: LoginData):
     """ function to obtain token lasting 1h, authenticating user to access his data """
-    session = mysql.connector.connect(
-        host="localhost",
-        port=3307,
-        database="Navigator",
-        user="root",
-        password="plsnohack"
-    )
-    
+    session = getDb()    
+
     cursor = session.cursor()
     sqlQuery = "SELECT id FROM Users where password=%s and login=%s"
     cursor.execute(sqlQuery,(loginData.password,loginData.login))
     rows = cursor.fetchall()
 
     if len(rows) > 0:
+        userId = rows[0][0]
         randomToken = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(64))
-        sqlQuery = "INSERT INTO Sessions (userId,loginDate,token,active) values (%s,NOW(),%s,TRUE)"
-        cursor.execute(sqlQuery,(rows[0][0],randomToken))
+        sqlQuery = "UPDATE Sessions SET active=0 WHERE userId = %s AND timestampdiff(HOUR,Sessions.loginDate,NOW())<2"
+        cursor.execute(sqlQuery,(userId,))
+        sqlQuery = "INSERT INTO Sessions (userId,loginDate,token,active) values (%s,NOW(),%s,1)"
+        cursor.execute(sqlQuery,(userId,randomToken))
         session.commit()
         session.close()
         return {"login_status":"success","token":randomToken,"userId":rows[0][0]}
 
     return {"login_status": "permission denied","token":"","userId":-1}
 
-@app.post("/monthlyreport")
-def get_monthly_report(tok: TokenData):
+@app.post("/testverificationtoken")
+def test_token(tok: TokenData):
     """ function to retrieve tables"""
     userId = getUserFromToken(tok.token)
     if userId == -1:
         return {"data":"reeeeee, get away from my data"}
-    return {"data":"here you go kind sir"}
+    return {"data":"here you go kind sir","userId":userId}
 
-
+class TableRequest(BaseModel):
+   token: str
+   month: str
+   year: str
+   
+@app.post("/monthlyreport")
+def monthly_report(req: TableRequest):
+    userId = getUserFromToken(req.token)
+    if userId == -1:
+        return {"status":"permission denied"}
+    db = getDb()
+    cursor = db.cursor()
+    fields = ["invoiceId","contractorType","contractorNip","idCardNumber","transactionDate","registrationDate","notes","pit17","pit15","pit12","pit10","pit85","pit55","pit03"]
+    brakcets = ["17","15","12.5","10","8.5","5.5","3"]
+    sqlQuery = "SELECT "+ ",".join(fields) + " FROM Invoices WHERE userId=%s"
+    cursor.execute(sqlQuery,(userId,))
+    rows = cursor.fetchall()
+    result = []
+    for row in rows:
+        i = 0
+        result.append(dict())
+        for val in row:
+            result[-1][fields[i]] = val
+            i+=1
+    return {"userId": userId,"table":result}
--- populator.py
+++ populator.py
@@ -15,13 +15,13 @@ def parseInvoices(filename):
     for line in lines:
         data = line.split(",")
         
-        contractorType = 2
+        contractorType = 1 
         if data[3].strip()[0].isnumeric() == False:
             contractorType = 2
         if data[3].strip() == 'osoba fizyczna' or data[3].strip() == 'os. fiz.':
             contractorType = 0
 
-        invoices.append("("+str(0) + ",'"+data[0]+"','"+data[1]+"','"+data[2]+"','"+ contractor_types[contractorType]+"','"+data[3]+"',"+str(id)+")")
+        invoices.append("("+str(0) + ",'"+data[0]+"','"+data[1]+"','"+data[2]+"','"+ contractor_types[contractorType]+"','"+data[3]+"',"+ ",".join([(x.replace(".","")) for x in data[4:11]])+","+str(id)+")")
         #print(invoices[-1])
         i = 0
         for value in data[4:11]:
@@ -33,23 +33,23 @@ def parseInvoices(filename):
     return invoices,items
 
 def fillDb():
-    navigator = mysql.connector.connect(
-        host="localhost",
-        database="Navigator",
-        user="root",
-        password="root"
-    )
-    mycursor = navigator.cursor()
+    # navigator = mysql.connector.connect(
+    #     host="localhost",
+    #     database="Navigator",
+    #     user="root",
+    #     password="root"
+    # )
+    # mycursor = navigator.cursor()
     invoices,items = parseInvoices("./data.csv")
 
     #print("INSERT INTO Navigator.Invoices (userId,registrationDate,transactionDate,idCardNumber,contractorNip,invoiceId) values " + ",".join(invoices))
-    print("INSERT INTO Invoices (userId,registrationDate,transactionDate,idCardNumber,contractorType,contractorNip,invoiceId) VALUES " + ",".join(invoices))
+    print("INSERT INTO Invoices (userId,registrationDate,transactionDate,idCardNumber,contractorType,contractorNip,pit17,pit15,pit12,pit10,pit85,pit55,pit03,invoiceId) VALUES " + ",".join(invoices))
     print("INSERT INTO Items (taxBracket,paymentValue,invoiceId) VALUES " + ",".join(items))
     print("INSERT INTO Users (login,password,name,surname,nip) values ('jp2','gmd','janusz','pavlacz','2137')")
     # mycursor.execute("INSERT INTO Invoices (userId,registrationDate,transactionDate,idCardNumber,contractorType,contractorNip,invoiceId) VALUES " + ",".join(invoices))
     # mycursor.execute("INSERT INTO Items (taxBracket,paymentValue,invoiceId) VALUES " + ",".join(items))
     # mycursor.execute("INSERT INTO Users (login,password,name,surname,nip) values ('jp2','gmd','janusz','pavlacz','2137')")
-    navigator.commit()
-    navigator.close()
+    # navigator.commit()
+    # navigator.close()
     
 fillDb()
