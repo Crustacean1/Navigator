bb0a713a234fa75d3d369273b90dfad62f5601ab
Kamil Kowalski <kamilkowalski314@gmail.com>
2021-12-12 01:37:56 +0100

    Disabled cors

--- api/app.py
+++ api/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Response, Cookie, Request
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Optional
 import mysql.connector
@@ -6,17 +7,52 @@ import random
 import string
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+    "http://localhost/",
+    "http://localhost:8000/",
+    "*"
+],
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=[""],
+)
 class LoginData(BaseModel):
     login: str
     password: str
+
+class TokenData(BaseModel):
+    token: str
+
+class TableRequest(BaseModel):
+    month: str
+    year: str
+
+def getUserFromToken(token: str):
+    session = mysql.connector.connect(
+        host="localhost",
+        port=3307,
+        database="Navigator",
+        user="root",
+        password="plsnohack"
+    )
+    cursor = session.cursor()
+    sqlQuery = "SELECT userId FROM Sessions WHERE token='dasfadsf'"#" AND timestampdiff(HOUR,Sessions.loginDate,NOW())<2"
+    cursor.execute(sqlQuery)#,("jpadfsasd"))
+    rows = cursor.fetchall()
+    if len(rows)==1:
+        return rows[0][0] 
+    return -1
+
 
 @app.get("/")
 def hello_world():
     return {"woo": "foo"}
 
 @app.get("/verification")
-def verify_user(loginData: LoginData,response: Response):
+def verify_user(loginData: LoginData):
+    """ function to obtain token lasting 1h, authenticating user to access his data """
     session = mysql.connector.connect(
         host="localhost",
         port=3307,
@@ -39,11 +75,13 @@ def verify_user(loginData: LoginData,response: Response):
         return {"login_status":"success","token":randomToken,"userId":rows[0][0]}
 
     return {"login_status": "permission denied"}
-@app.get("/user_profile")
 
-def display_profile(request: Request, user_login: Optional[str] = Cookie(None)):
-    if user_login:
-        return {"user_password":user_login}
-    return {"user_login":"None :("}
+@app.get("/monthlyreport")
+def get_monthly_report(tok: TokenData):
+    """ function to retrieve tables"""
+    userId = getUserFromToken(tok.token)
+    if userId == -1:
+        return {"data":"reeeeee, get away from my data"}
+    return {"data":"here you go kind sir"}
 
 
